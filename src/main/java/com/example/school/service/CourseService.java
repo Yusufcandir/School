615227d77b734500533af4797fa3e481a8119f98
Yusuf Candır <yusufcandir30@gmail.com>
2022-10-11 16:37:01 +0300

    CourseService class has been added

---
+++ src/main/java/com/example/school/service/CourseService.java
@@ -0,0 +1,41 @@
+package com.example.school.service;
+
+import com.example.school.dto.CourseDto;
+
+import com.example.school.dto.converter.CourseDtoConverter;
+import com.example.school.exception.CourseNotFoundException;
+import com.example.school.model.Course;
+
+import com.example.school.repository.CourseRepository;
+
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+public class CourseService {
+
+    private final CourseRepository repository;
+    private final CourseDtoConverter converter;
+
+    public CourseService(CourseRepository repository, CourseDtoConverter converter) {
+        this.repository = repository;
+        this.converter = converter;
+    }
+
+    public Course findCourseById(String id){
+        return repository.findById(id).
+                orElseThrow(()-> new CourseNotFoundException("Course could not found by id: "+ id));
+    }
+
+
+    public CourseDto getCourseById(String id){
+        return converter.convert(findCourseById(id));
+    }
+
+    public List<CourseDto> getAllStudents(){
+        return repository.findAll().stream()
+                .map(converter::convert).collect(Collectors.toList());
+    }
+}
