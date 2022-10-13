9718f9f0df325381d5a592a4b289109fb3c651b6
Yusuf Candır <yusufcandir30@gmail.com>
2022-10-13 18:06:07 +0300

    created StudentServiceTest and added setUp method.

---
+++ src/test/java/com/example/school/service/StudentServiceTest.java
@@ -0,0 +1,30 @@
+package com.example.school.service;
+
+import com.example.school.dto.converter.StudentDtoConverter;
+import com.example.school.repository.StudentRepository;
+import org.junit.jupiter.api.BeforeEach;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.mockito.Mockito;
+
+import static org.mockito.Mockito.mock;
+
+class StudentServiceTest {
+
+    @Autowired
+    private StudentService service;
+    @Autowired
+    private StudentRepository repository;
+    @Autowired
+    private StudentDtoConverter converter;
+
+    @BeforeEach
+    public void setUp(){
+       repository=mock(StudentRepository.class);
+       converter=mock(StudentDtoConverter.class);
+       service= new StudentService(repository,converter);
+    }
+
+
+
+}
