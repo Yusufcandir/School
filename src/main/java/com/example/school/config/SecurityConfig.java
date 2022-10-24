96ca5fc74b3e1e3bd45ff57bc3a232d3fc75f93d
Yusuf Candır <yusufcandir30@gmail.com>
2022-10-24 20:36:27 +0300

    Some changes have been made in SecurityConfig

--- src/main/java/com/example/school/config/SecurityConfig.java
+++ src/main/java/com/example/school/config/SecurityConfig.java
@@ -1,9 +1,13 @@
 package com.example.school.config;
 
+import com.example.school.service.StudentService;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.core.userdetails.User;
@@ -18,6 +22,15 @@ import org.springframework.security.web.SecurityFilterChain;
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
+
+
+    private final PasswordEncoder encoder;
+    private final StudentService service;
+
+    public SecurityConfig(PasswordEncoder encoder, StudentService service) {
+        this.encoder = encoder;
+        this.service = service;
+    }
 
     @Bean
     public InMemoryUserDetailsManager userDetailsManager(){
@@ -44,7 +57,7 @@ public class SecurityConfig {
         return http.csrf(csrf-> csrf.disable())
                 .authorizeRequests(auth->
                 {
-                    auth.antMatchers("/").permitAll();
+                    auth.antMatchers("/api/v*/registration/**", "/h2/**", "/h2/*", "/h2-console/**", "/h2-console/*").permitAll();
 
                     auth.antMatchers(HttpMethod.GET,"/v1/instructor/**").hasAnyRole("ADMIN","USER");
                     auth.antMatchers(HttpMethod.POST,"/v1/instructor/**").hasRole("ADMIN");
@@ -61,5 +74,18 @@ public class SecurityConfig {
                 .build();
 
 
+    }
+
+    @Bean
+    protected void configure(AuthenticationManagerBuilder builder){
+     builder.authenticationProvider(provider());
+    }
+
+    @Bean
+    DaoAuthenticationProvider provider(){
+        DaoAuthenticationProvider provider= new DaoAuthenticationProvider();
+        provider.setPasswordEncoder(encoder.bCryptPasswordEncoder());
+        provider.setUserDetailsService(service);
+        return provider;
     }
 }
